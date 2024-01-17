9fd793c83bb803cca65e56132f292a28d4a6fb85
Raxmoanli <raxmonaliabduvaliyeve077@gmail.com>
2024-01-17 17:42:14 +0500

    frist

---
+++ app.js
@@ -0,0 +1,23 @@
+// "use strict";
+// const place = confirm ("are you wait here me");
+// console.log (place); //confirm : true and folse return;
+
+// const currentPlace = prompt("where are you man", "ali"); // currentPlace;
+
+// const age = +prompt("how old are you new people", "19");
+// alert( typeof age);
+
+// const favouriteColor = [];
+
+// favouriteColor[0] = prompt("what`s your fovourite color #1","");
+// favouriteColor[1] = prompt("what`s your fovourite color #2","");
+// favouriteColor[2] = prompt("what`s your fovourite color #3","");
+// console.log(favouriteColor);    
+
+// const channel = prompt("what`s your farourite youtube");
+// console.log(`https://youtube.com/${channel }`); //interpolatsiya;
+
+// const user = "ali";
+// const job = "javaScript developer";
+//                                                    //interpolatsiya;
+// console.log(`user name is ${user} he is ${job}`);
